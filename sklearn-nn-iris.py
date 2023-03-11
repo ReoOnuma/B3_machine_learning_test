52d2faed742eca9a10c05033bd9c66777d8a960f
aktkszk000 <hibifusi62@gmail.com>
2023-03-11 17:06:54 +0900

    「Step3: モデルの作成」了

--- sklearn-nn-iris.py
+++ sklearn-nn-iris.py
@@ -15,3 +15,7 @@ df_iris = pd.DataFrame(data=iris.data, columns=iris.feature_names)
 df_iris['target'] = iris.target
 # split the dataset
 data_train, data_test, target_train, target_test = train_test_split(iris.data, iris.target, test_size=0.2, random_state=0)
+
+# define neural network model
+clf = MLPClassifier(hidden_layer_sizes=10, activation='relu', solver='adam', max_iter=1000)
+
