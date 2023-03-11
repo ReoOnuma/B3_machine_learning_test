c458019bcf3ddca30a5dbeac5b3efc1da3f6e14c
aktkszk000 <hibifusi62@gmail.com>
2023-03-11 17:01:02 +0900

    「Step2: データの準備」了

--- sklearn-nn-iris.py
+++ sklearn-nn-iris.py
@@ -5,3 +5,13 @@ import matplotlib.pyplot as plt
 from sklearn import datasets
 from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import train_test_split
+
+
+# load datasets
+iris = datasets.load_iris()
+# load as pandas.DataFrame
+df_iris = pd.DataFrame(data=iris.data, columns=iris.feature_names)
+# add target
+df_iris['target'] = iris.target
+# split the dataset
+data_train, data_test, target_train, target_test = train_test_split(iris.data, iris.target, test_size=0.2, random_state=0)
